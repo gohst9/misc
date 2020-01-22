f480b7e5d0909b0d66f468ff8871555c9e824abc
gohst9 <blackgohstno9@yahoo.co.jp>
2020-01-22 22:52:21 +0900

    テンパイ判定機能追加

--- mahjong.py
+++ mahjong.py
@@ -42,6 +42,50 @@ class Hand:
 
         return hand
 
+    def is_tempai(self):
+        manzu = self.manzu
+        pinzu = self.pinzu
+        souzu = self.souzu
+        suits = [manzu,pinzu,souzu]
+        shunts = 0
+        kots = 0
+        toits = 0
+
+        #順子の数をカウント
+        for s in suits:
+            for i in range(1,8):
+                while s[i]>=1 and s[i+1]>=1 and s[i+2]>=1:
+                    s[i]-=1
+                    s[i+1]-=1
+                    s[i+2]-=1
+                    shunts += 1
+
+        #刻子の数をカウント
+        for s in suits:
+            for i in range(1,10):
+                while s[i] >= 3:
+                    s[i] -= 3
+                    kots += 1
+
+        #対子の数をカウント
+        for s in suits:
+            for i in range(1,10):
+                while s[i] >= 2:
+                    s[i] -= 2
+                    toits += 1
+
+        print("順子の数:",shunts)
+        print("刻子の数",kots)
+        print("対子の数",toits)
+        tempai = (shunts + kots) == 4 and toits == 1
+        print("テンパイ","しています" if tempai else "していません")
+
+
+
+
+
+
+
     def show(self):
         print(self.hand_to_s())
 
@@ -56,3 +100,4 @@ if __name__ == '__main__':
     s = input("123m456p789s形式で麻雀の手牌を入力してください")
     hand = Hand(s)
     hand.show()
+    hand.is_tempai()
