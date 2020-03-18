8e0954ed403b24212d91a42a568bb217491ef148
gohst9 <blackgohstno9@yahoo.co.jp>
2020-03-18 20:29:01 +0900

    全角文字列が入ってたので修正

--- grandcross.py
+++ grandcross.py
@@ -1,15 +1,15 @@
 import pyperclip
 
 
-copy_text = pyperclip.paste()　#クリップボードから文字列を取得
-center = len(s1) // 2 #文字列の中央の位置を求める（適当）
+copy_text = pyperclip.paste()#クリップボードから文字列を取得
+center = len(copy_text) // 2#文字列の中央の位置を求める（適当）
 output_text_lst = [] #後でjoinして出力するための文字列リスト
 
 for i,c in enumerate(copy_text):
     if i != center:
         output_text_lst.append(("　" * center) + c)
     else:
-        output_text_lst.append(s1)
+        output_text_lst.append(copy_text)
 
 output_text = "\n".join(output_text_lst)
 print(output_text)
