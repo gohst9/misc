b3f632d6e5a5be2a32b16c7c2246e34f55c4749e
gohst9 <blackgohstno9@yahoo.co.jp>
2020-03-18 20:26:37 +0900

    クリップボードの文字列をグランドクロス文字列に変換してクリップボードに格納

---
+++ grandcross.py
@@ -0,0 +1,17 @@
+import pyperclip
+
+
+copy_text = pyperclip.paste()　#クリップボードから文字列を取得
+center = len(s1) // 2 #文字列の中央の位置を求める（適当）
+output_text_lst = [] #後でjoinして出力するための文字列リスト
+
+for i,c in enumerate(copy_text):
+    if i != center:
+        output_text_lst.append(("　" * center) + c)
+    else:
+        output_text_lst.append(s1)
+
+output_text = "\n".join(output_text_lst)
+print(output_text)
+pyperclip.copy(output_text) #クリップボードにグランドクロス文字列を張り付ける
+
