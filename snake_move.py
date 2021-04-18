51d718495d539d1f9b53bc4c9f389be96210e759
gohst9 <blackgohstno9@yahoo.co.jp>
2021-04-18 13:36:43 +0900

    完成版

--- snake_move.py
+++ snake_move.py
@@ -9,6 +9,8 @@
 import pyxel
 import random
 
+BODY_COLOR = 10
+
 class Body:
     def __init__(self,x,y,tail = None):
         self.x = x
@@ -22,7 +24,7 @@ class Body:
         if self.tail and (dx != 0 or dy != 0):
             self.tail.move(pre_x - self.tail.x,pre_y - self.tail.y)
     def draw(self):
-        pyxel.rect(self.x,self.y,1,1,1)
+        pyxel.rect(self.x,self.y,1,1,BODY_COLOR)
         if self.tail:
             self.tail.draw()
 
@@ -48,19 +50,38 @@ class App:
         self.head = Body(pyxel.width//2,pyxel.height//2)
         self.is_game_over = False
         #エサの初期配置
-        self.food_x = random.randint(0,pyxel.width)
-        self.food_y = random.randint(0,pyxel.height)
+        self.food_x = random.randint(0,pyxel.width-1)
+        self.food_y = random.randint(0,pyxel.height-1)
         self.dx = 0
         self.dy = 0
-
-
+        self.length = 1
         pyxel.run(self.update, self.draw)
 
     def draw_food(self):
-        pyxel.rect(self.food_x,self.food_y,1,1,1)
+        pyxel.rect(self.food_x,self.food_y,1,1,BODY_COLOR)
 
     def update(self):
-        self.dx,self.dy = 0,0 #デバッグ用
+        #ゲームスピード調整
+        if pyxel.frame_count % 2 != 0:
+            return
+
+        #self.dx,self.dy = 0,0 #デバッグ用
+
+        if self.is_game_over:
+            if pyxel.btn(pyxel.KEY_R):
+                self.head = Body(pyxel.width//2,pyxel.height//2)
+                self.is_game_over = False
+                self.food_x = random.randint(0,pyxel.width-1)
+                self.food_y = random.randint(0,pyxel.height-1)
+                self.dx = 0
+                self.dy = 0
+                self.length = 1
+            if pyxel.btn(pyxel.KEY_Q):
+                pyxel.quit()
+            return
+
+
+        #self.dx,self.dy = 0,0 #デバッグ用
         #入力受け取り
         if pyxel.btn(pyxel.KEY_LEFT):
             self.dx,self.dy = -1,0
@@ -88,8 +109,9 @@ class App:
         #エサを食べられるかどうかの判定
         if self.head.x + self.dx == self.food_x and self.head.y + self.dy == self.food_y:
             self.head = Body(self.head.x+self.dx,self.head.y+self.dy,self.head)
-            self.food_x = random.randint(0,pyxel.width)
-            self.food_y = random.randint(0,pyxel.height)
+            self.food_x = random.randint(0,pyxel.width-1)
+            self.food_y = random.randint(0,pyxel.height-1)
+            self.length += 1
 
 
         self.head.move(self.dx,self.dy)
@@ -97,8 +119,14 @@ class App:
     def draw(self):
         pyxel.cls(0)
         if self.is_game_over:
-            pyxel.text(0,0,"game over",10)
+            pyxel.text(0,0,"game over press R to retry",10)
+            pyxel.text(pyxel.width//2 -16,pyxel.height//2,"YOUR SCORE:"+str(self.length),pyxel.frame_count//10%15+1)
+            return
+        pyxel.text(0,0,"LENGTH:"+str(self.length),7)
         self.head.draw()
         self.draw_food()
+
+
+
 
 App()
