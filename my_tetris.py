2bc611afc5ba9f29a2465eb4582a261e32f39d08
gohst9 <blackgohstno9@yahoo.co.jp>
2021-07-02 20:37:07 +0900

    ミノ1種類テトリス　todo:端での回転操作、解像度をあげる

--- my_tetris.py
+++ my_tetris.py
@@ -1,25 +1,65 @@
 import pyxel
 
-shape1 = [[0,0,0,0],
+shapes =[[[0,0,0,0],
           [0,1,0,0],
           [1,1,1,0],
-          [0,0,0,0]]
+          [0,0,0,0]],
+
+         [[0,0,0,0],
+          [0,1,0,0],
+          [0,1,1,0],
+          [0,1,0,0]],
+
+         [[0,0,0,0],
+          [0,0,0,0],
+          [1,1,1,0],
+          [0,1,0,0]],
+
+         [[0,0,0,0],
+          [0,1,0,0],
+          [1,1,0,0],
+          [0,1,0,0]
+          ]
+        ]
+WIDTH = 8
+HEIGHT = 12
 
 class Board:
     def __init__(self,width,height):
+        self.game_over = False
         self.width = width
         self.height = height
         self.board = [[0 for _ in range(width)] for _ in range(height)]
         self.block_start_x = width//2
-        self.block_start_y = 10
-        self.block = Block(self.block_start_x,self.block_start_y,shape1)
+        self.block_start_y = -2
+        self.block = Block(self.block_start_x,self.block_start_y,shapes)
+        self.score = 0
 
     def update(self):
-        if self.block.is_fallable(self.board):
-            self.block.fall()
+        self.board,point = delete_full_line(self.board)
+        self.score += point
+        speed_up = pyxel.btn(pyxel.KEY_S)
+        dx = pyxel.btnp(pyxel.KEY_D) - pyxel.btnp(pyxel.KEY_A)
+        if pyxel.btnp(pyxel.KEY_W) and self.block.is_rotatable(self.board):
+            self.block.rotate()
+        if self.block.is_slidable(dx,self.board):
+            self.block.x += dx
+        if self.block.is_fallable(self.board) :
+            if speed_up or pyxel.frame_count%10==0:
+                self.block.fall()
         else:
             self.block.fix(self.board)
             self.create_new_block()
+            for i in range(self.block.height):
+                for j in range(self.block.width):
+                    if self.block_start_y + i < 0:
+                        continue
+                    if self.board[self.block.y + i][self.block.x + j] and self.block.shape[i][j]:
+                        if not self.game_over:print("Game Over,score:",self.score)
+                        self.game_over = True
+
+
+
 
     def draw(self):
         self.block.draw()
@@ -28,36 +68,52 @@ class Board:
                 if self.board[i][j]:
                     pyxel.rect(j,i,1,1,9)
 
+
     def create_new_block(self):
-        self.block = Block(self.block_start_x,self.block_start_y,shape1)
+        self.block = Block(self.block_start_x,self.block_start_y,shapes)
 
 class Block:
-    def __init__(self,x:int,y:int,shape:list):
+    def __init__(self,x:int,y:int,shapes:list):
         self.x = x #左端
         self.y = y #上端
-        self.height = len(shape)
-        self.width = len(shape[0])
-        self.shape = shape
+        self.shapes = shapes
+        self.rotate_n = 0
+        self.shape = self.shapes[self.rotate_n]
+        self.height = len(self.shape)
+        self.width = len(self.shape[0])
+
 
     def update(self,fall_flag:bool):
-        dx = pyxel.btn(pyxel.KEY_D) - pyxel.btn(pyxel.KEY_A)
-        self.x += dx * self.is_slidable(dx,lst)
-        if pyxel.frame_count % 10 == 0:
-            if fall_flag:
-                self.fall()
+        pass
+
+    def is_rotatable(self,board):
+        new_shape = self.shapes[(self.rotate_n + 1)%4]
+        for i in range(self.height):
+            for j in range(self.width):
+                if new_shape[i][j] and board[self.y + i][self.x + j]:
+                    return False
+                if not (0 <= self.y + i < len(board)) or not (0 <= self.x + j < len(board[0])):
+                    return False
+        return True
+
+
+
+    def rotate(self):
+        self.rotate_n = (self.rotate_n + 1) % 4
+        self.shape = self.shapes[self.rotate_n]
 
     def fix(self,lst):
         for i in range(self.height):
             for j in range(self.width):
                 assert i < len(self.shape)
-                assert j < len(self.shape[0])             #fix、もしくはis_fallableが間違っていて、ブロックがすでに配置されたブロックの上にめり込む
+                assert j < len(self.shape[0])
                 fix_x = self.x + j
                 fix_y = self.y + i
                 if not self.shape[i][j]:
                     continue
                 assert fix_y < len(lst)
                 assert fix_x < len(lst[0])
-                lst[fix_y][fix_x] = True
+                lst[fix_y][fix_x] = 1
 
 
     def draw(self):
@@ -73,13 +129,22 @@ class Block:
         self.y += 1
 
     def is_slidable(self,dx,lst):
-        return 0 <= self.x + dx < len(lst[0])
+        board_width = len(lst[0])
+        for i in range(self.height):
+            for j in range(self.width):
+                if not self.shape[i][j]:
+                    continue
+                if not (0 <= self.x + j + dx < board_width):
+                    return False
+                if lst[self.y + i][self.x + j + dx]:
+                    return False
+        return True
 
     def is_fallable(self,lst):
         for i in range(self.height):
             for j in range(self.width):
                 #そのマス目が空白の場合、無視して続行
-                if not self.shape[j][i]:
+                if not self.shape[i][j]:
                     continue
                 nxt_x = self.x + j
                 nxt_y = self.y + i + 1
@@ -91,6 +156,7 @@ class Block:
 
 
 def delete_full_line(board):
+    width = len(board[0])
     del_count = 0
     new_board = []
     for line in board:
@@ -99,21 +165,30 @@ def delete_full_line(board):
             del_count += 1
         else:
             new_board.append(line)
+    new_board = [[0 for _ in range(width)] for _ in range(del_count)] + new_board
     board = new_board #ボードを新しいボードに差し替え
-    return del_count #消した行の数を返却（スコア計算用）
+    return new_board,del_count
 
 
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
-        self.board = Board(160,120)
+        pyxel.init(WIDTH, HEIGHT)
+        self.board = Board(WIDTH,HEIGHT)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.board.update()
+        if not self.board.game_over:
+            self.board.update()
+        else:
+            if pyxel.btnp(pyxel.KEY_Q):
+                pyxel.quit()
 
     def draw(self):
         pyxel.cls(0)
+        if self.board.game_over:
+            pyxel.text(0,0,"><",7)
+            return
+
         self.board.draw()
 App()
